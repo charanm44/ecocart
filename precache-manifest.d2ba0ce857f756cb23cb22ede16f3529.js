e1109020623255e829b913cd1ada8e572aa1865a
theGeekyLad <rahul.pillai03@gmail.com>
2020-09-18 00:13:35 +0530

    Updates

---
+++ precache-manifest.d2ba0ce857f756cb23cb22ede16f3529.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9673361c82bbe9330d82d1976bf477d4",
+    "url": "/ecocart/index.html"
+  },
+  {
+    "revision": "cc1f65dd7fee4b762c3c",
+    "url": "/ecocart/static/css/main.50cca853.chunk.css"
+  },
+  {
+    "revision": "c7ecc92d406f8808d90c",
+    "url": "/ecocart/static/js/2.18e111da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecocart/static/js/2.18e111da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1f65dd7fee4b762c3c",
+    "url": "/ecocart/static/js/main.8b7ff6d3.chunk.js"
+  },
+  {
+    "revision": "9e3596c256c8349058a5",
+    "url": "/ecocart/static/js/runtime-main.55ccef53.js"
+  },
+  {
+    "revision": "a87721b945a8039392bcf8a568cc5939",
+    "url": "/ecocart/static/media/airplane.a87721b9.png"
+  },
+  {
+    "revision": "04f8ab9eeacb865b5aa81bb9a05d3231",
+    "url": "/ecocart/static/media/car.04f8ab9e.png"
+  },
+  {
+    "revision": "bfc48df3efbc003f369b28cfbe5915a6",
+    "url": "/ecocart/static/media/car_electric.bfc48df3.png"
+  },
+  {
+    "revision": "380539e692ddaabdb4a73d7eb2c75915",
+    "url": "/ecocart/static/media/co2.380539e6.png"
+  },
+  {
+    "revision": "001eb7b33008f775d0b619f0d5ea867a",
+    "url": "/ecocart/static/media/diet_type.001eb7b3.png"
+  },
+  {
+    "revision": "642090710adac69f6e91604a1aab6d38",
+    "url": "/ecocart/static/media/heating_freq.64209071.png"
+  },
+  {
+    "revision": "b83b55e8928acd8d64fda426d6267350",
+    "url": "/ecocart/static/media/home.b83b55e8.png"
+  },
+  {
+    "revision": "665b810c276717ae4b2079cb0e40336c",
+    "url": "/ecocart/static/media/live.665b810c.png"
+  },
+  {
+    "revision": "46d039d86bc1ca7dc7db63521fdffabc",
+    "url": "/ecocart/static/media/loading.46d039d8.png"
+  },
+  {
+    "revision": "d4b8cd6e89b6c593ea0654851832b878",
+    "url": "/ecocart/static/media/location.d4b8cd6e.png"
+  },
+  {
+    "revision": "18f2d56de1b8519048e9b48f517b2d62",
+    "url": "/ecocart/static/media/new_stuff.18f2d56d.png"
+  },
+  {
+    "revision": "79f8ec460e0b11cbbff927a3ba6ba8b5",
+    "url": "/ecocart/static/media/people_count.79f8ec46.png"
+  },
+  {
+    "revision": "b91226ea7f390f6eb2d64ea8b25b2f9f",
+    "url": "/ecocart/static/media/sprout_1.b91226ea.png"
+  },
+  {
+    "revision": "faf311bf36da12a0ab8a6b9c27c158b3",
+    "url": "/ecocart/static/media/sprout_2.faf311bf.png"
+  },
+  {
+    "revision": "14aada8eeb0c41b304548a5cc16fc5ee",
+    "url": "/ecocart/static/media/stuff_count.14aada8e.png"
+  },
+  {
+    "revision": "45f0b4177e36f05a455377c78f4eaf18",
+    "url": "/ecocart/static/media/tree.45f0b417.png"
+  },
+  {
+    "revision": "d1d797708f44fef16a3c85102ce2e110",
+    "url": "/ecocart/static/media/tree_icon.d1d79770.png"
+  }
+]);
